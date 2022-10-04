e18a87f8d0f2aa70632f3508da794418e91d3ed2
SergeiGorkun <sergeigorkun1992@yandex.ru>
2022-10-04 22:19:43 +0600

    task 8

---
+++ Задание 8/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new Random();
+
+int value = rnd.Next(10, 99);
+ 
+Console.WriteLine(value);
+
+string str = Convert.ToString(value);
+
+char a = str[0];
+char b = str[1];
+
+if(a > b) {
+    Console.WriteLine(a);
+} else {
+    Console.WriteLine(b);
+}
