13feadf3831bcc7b541f59f8844c7049220f9ed3
SergeiGorkun <sergeigorkun1992@yandex.ru>
2022-10-04 22:49:32 +0600

    task 11

---
+++ Задание 11/Program.cs
@@ -0,0 +1,11 @@
+﻿Random rnd = new Random();
+int value = rnd.Next(1, 200);
+Console.WriteLine(value);
+
+string str = Convert.ToString(value);
+
+if(str.Length > 2) {
+    Console.WriteLine(str[2]);
+} else {
+    Console.WriteLine("Третья цифра отутствует");
+}
